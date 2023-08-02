f0366f5a72b739124c0e3e224833ad2834451e4f
Md Rakib Hossen <rakib@Mds-MacBook-Pro.local>
2023-08-02 10:38:52 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {  
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 0, 0, 0),
+              Color.fromARGB(255, 181, 181, 181)
+            ],
+            begin: Alignment.topLeft,
+            end:Alignment.bottomRight,
+            ),
+          ),
+          child: const Center(
+            child: Text("Rakibhhhhjjjjhhh"),
+          ),
+        ),
+      ),
+    ),
+  );
+}
